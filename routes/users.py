389ed480bff896402a116f2cbc46020bdfc3f48e
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-20 21:18:41 -0400

    add response model like ReponseBase to create user and update user

--- routes/users.py
+++ routes/users.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, status
 from typing import List
 from config.db import conn
 from models.user import users
+from schemas.response import ResponseBase
 from schemas.user import User
+from helpers.response import create_response
 
 from cryptography.fernet import Fernet
 
@@ -15,28 +17,29 @@ user_routes = APIRouter()
 def get_users():
     return conn.execute(users.select()).fetchall()
 
-@user_routes.post("/users", response_model=User, status_code=status.HTTP_201_CREATED, tags=["users"])
+@user_routes.post("/users", response_model=ResponseBase[User], status_code=status.HTTP_201_CREATED, tags=["users"])
 def create_user(user: User):
     new_user = {
         "name": user.name,
         "email": user.email
     }
     new_user["password"] = f.encrypt(user.password.encode("utf-8")).decode("utf-8")
-    print(new_user)
     result = conn.execute(users.insert(), new_user)
-    print(result)
-    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+    return create_response(
+        message="User created successfully",
+        data=conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+    )
 
 @user_routes.get("/users/{id}", response_model=User, status_code=status.HTTP_200_OK, tags=["users"])
 def get_user(id: int):
     return conn.execute(users.select().where(users.c.id == id)).first()
 
-@user_routes.delete("/users/{id}", tags=["users"])
+@user_routes.delete("/users/{id}", tags=["users"], response_model=ResponseBase, status_code=status.HTTP_200_OK)
 def delete_user(id: int):
     conn.execute(users.delete().where(users.c.id == id))
-    return {"message": "User deleted"}
+    return create_response(message="User deleted successfully", data=None)
 
-@user_routes.put("/users/{id}", response_model=User, status_code=status.HTTP_200_OK, tags=["users"])
+@user_routes.put("/users/{id}", response_model=ResponseBase[User], status_code=status.HTTP_200_OK, tags=["users"])
 def update_user(id: int, user: User):
     new_user = {
         "name": user.name,
@@ -44,8 +47,7 @@ def update_user(id: int, user: User):
         "password": f.encrypt(user.password.encode("utf-8")).decode("utf-8")
     }
     conn.execute(users.update().where(users.c.id == id), new_user)
-    return {
-        "message": "User updated",
-        "data": conn.execute(users.select().where(users.c.id == id)).first()
-    }
-    
+    return create_response(
+        message="User updated successfully",
+        data=conn.execute(users.select().where(users.c.id == id)).first()
+    )    
