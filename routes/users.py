ac7e8ae4280cb2fbd36be590ba102c6abce73a7e
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-20 20:37:03 -0400

    add status code to get all, get user, create user and update user

--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from typing import List
 from config.db import conn
 from models.user import users
@@ -11,11 +11,11 @@ f = Fernet(key)
 
 user_routes = APIRouter()
 
-@user_routes.get("/users", response_model=List[User])
+@user_routes.get("/users", response_model=List[User], status_code=status.HTTP_200_OK)
 def get_users():
     return conn.execute(users.select()).fetchall()
 
-@user_routes.post("/users", response_model=User)
+@user_routes.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
 def create_user(user: User):
     new_user = {
         "name": user.name,
@@ -27,7 +27,7 @@ def create_user(user: User):
     print(result)
     return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
 
-@user_routes.get("/users/{id}", response_model=User)
+@user_routes.get("/users/{id}", response_model=User, status_code=status.HTTP_200_OK)
 def get_user(id: int):
     return conn.execute(users.select().where(users.c.id == id)).first()
 
@@ -36,7 +36,7 @@ def delete_user(id: int):
     conn.execute(users.delete().where(users.c.id == id))
     return {"message": "User deleted"}
 
-@user_routes.put("/users/{id}", response_model=User)
+@user_routes.put("/users/{id}", response_model=User, status_code=status.HTTP_200_OK)
 def update_user(id: int, user: User):
     new_user = {
         "name": user.name,
