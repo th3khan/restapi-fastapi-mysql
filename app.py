4366ca9903cfeb7f239624012c3661fb3ddf2d7a
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-20 20:41:53 -0400

    add documentation for API

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ from fastapi import FastAPI
 
 from routes.users import user_routes
 
-app = FastAPI()
+app = FastAPI(
+    title="My FastAPI App",
+    description="This is my FastAPI App",
+    version="0.1.0",
+    openapi_tags=[
+        {
+            "name": "users",
+            "description": "Operations about users"
+        }
+    ]
+)
 app.include_router(user_routes)
